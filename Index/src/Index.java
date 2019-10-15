baceed7e1d0a04c51d045cfd9306484775685652
IMalyanova <53153912+IMalyanova@users.noreply.github.com>
2019-10-15 18:58:49 +0300

    1

---
+++ Index/src/Index.java
@@ -0,0 +1,1 @@
+public class Index {    public static void main(String[] args) {       for (int i = 0; i <= 1200; i ++){           char k =(char)i;           System.out.println(i + ": " + k);// русские буквы: 1105, 1025, 1040-1103       }       char с = 'Ё';       char v = 'ё';       System.out.println((int)с);       System.out.println((int)v);    }}
